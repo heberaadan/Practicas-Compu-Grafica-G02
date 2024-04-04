54783da2107da127dade882eb35509580de9db58
Heber Adan <heberadan.pilaresgarcia@gmail.com>
2024-04-03 23:12:11 -0600

    Primer Ejercicio Terminado

--- P06-318122131.cpp
+++ P06-318122131.cpp
@@ -5,10 +5,13 @@ Práctica 6: Texturizado
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <stdio.h>
+#include <iostream>
+#include <thread>
 #include <string.h>
 #include <cmath>
 #include <vector>
 #include <math.h>
+#include <chrono>
 
 #include <glew.h>
 #include <glfw3.h>
@@ -42,8 +45,7 @@ Texture plainTexture;
 Texture pisoTexture;
 Texture dadoTexture;
 Texture logofiTexture;
-Texture dadon,dado_A;
-Texture dadonT;
+Texture Dado;
 
 Model Kitt_M;
 Model Llanta_M;
@@ -167,6 +169,7 @@ void CreateObjects()
 }
 
 
+
 void CreateShaders()
 {
 	Shader *shader1 = new Shader();
@@ -177,93 +180,112 @@ void CreateShaders()
 void CrearDado()
 {
 	unsigned int cubo_indices[] = {
-		// E(0), G(1), J(2)
+		// E, J, G, B
 		0, 1, 2,
+		0, 3, 2,
 
-		// G, J, K(3)
-		1, 2, 3,
+		// G, J, K, D
+		4, 5, 6,
+		4, 7, 6,
 
-		// K, J, F(4)
-		3, 2, 4,
+		// K, J, F, C
+		8, 9, 10,
+		8, 11, 10,
 
-		// F, J, H(5)
-		4, 2, 5,
+		// F, J, H, O
+		12, 13, 14,
+		12, 15, 14,
 
-		// H, J, E
-		5, 2, 0,
+		// H, J, E, A
+		16, 17, 18,
+		16, 19, 18,
 
-		// E, G, P(6)
-		0, 1, 6,
+		// B, E, A, P
+		20, 21, 22,
+		20, 22, 23,
 
-		// G, P, K(3)
-		1, 6, 3,
+		// D, G, B, P
+		24, 25, 26,
+		24, 26, 27,
 
-		// K, P, F(4)
-		3, 6, 4,
+		// C, K, D, P
+		28, 29, 30,
+		28, 30, 31,
 
-		// F, P, H(5)
-		4, 6, 5,
+		// O, F, C, P
+		32, 33, 34,
+		32, 34, 35,
 
-		// H, P, E
-		5, 6, 0,
+		// A, H, O, P
+		36, 37, 38,
+		36, 38, 39
 	};
-	
+
 	GLfloat cubo_vertices[] = {
-		
-		//x		y		z		S		T			NX		NY		NZ
-		0.0f, 0.0f,  3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// E
-		-3.0f, 0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// G
-		0.0f,  2.5f,  0.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// J
-		
-		//x		y		z		S		T
-		-3.0f, 0.0f,  0.5f,	    0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// G
-		0.0f,  2.5f,  0.0f,		0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// J
-		-1.5f, 0.0f,  -3.0f,	0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// K
-
-		//x		y		z		S		T
-		-1.5f, 0.0f,  -3.0f,	0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// K
-		0.0f,  2.5f,  0.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// J
-		1.5f,  0.0f, -3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// F
-
-		//x		y		z		S		T
-		1.5f,  0.0f, -3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// F
-		0.0f,  2.5f,  0.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// J 
-		3.0f,  0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// H
-
-		//x		y		z		S		T
-		3.0f,  0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// H
-		0.0f,  2.5f,  0.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// J
-		0.0f, 0.0f,  3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// E
-
 
 		//x		y		z		S		T			NX		NY		NZ
-		0.0f, 0.0f,  3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// E
-		-3.0f, 0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// G
-		0.0f,  -2.5f,  0.0f,	0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// P
+		0.0f, 0.0f,  3.0f,		0.23f,  0.82f,		-1.0f,	1.0f,	1.0f,	// E (0)
+		0.0f,  2.5f,  0.0f,		0.115f,  0.99f,		-1.0f,	1.0f,	1.0f,	// J (2)
+		-3.0f, 0.0f,  0.5f,		0.0f,  0.82f,		-1.0f,	1.0f,	1.0f,	// G (1)
+		-2.0f, -0.7f, 2.0f,		0.115f,  0.70f,		-1.0f,	1.0f,	1.0f,	// B 																
 
 		//x		y		z		S		T
-		-3.0f, 0.0f,  0.5f,	    0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// G
-		0.0f,  -2.5f,  0.0f,	0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// P
-		-1.5f, 0.0f,  -3.0f,	0.0f,  0.0f,		-1.0f,	0.0f,	0.0f,	// K
+		-3.0f, 0.0f,  0.5f,	    0.48f,  0.82f,		-1.0f,	1.0f,	-1.0f,	// G 
+		0.0f,  2.5f,  0.0f,		0.375f,  0.99f,		-1.0f,	1.0f,	-1.0f,	// J (5)
+		-1.5f, 0.0f,  -3.0f,	0.27f,  0.82f,		-1.0f,	1.0f,	-1.0f,	// K (6)
+		-3.0f, -0.7f,  -2.0f,	0.375f,  0.70f,		-1.0f,	1.0f,	-1.0f,	// D 
 
 		//x		y		z		S		T
-		-1.5f, 0.0f,  -3.0f,	0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// K
-		0.0f,  -2.5f,  0.0f,	0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// P
-		1.5f,  0.0f, -3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// F
+		-1.5f, 0.0f,  -3.0f,	0.72f,  0.82f,		0.0f,	1.0f,	-1.0f,	// K 
+		0.0f,  2.5f,  0.0f,		0.625f,  0.99f,		0.0f,	1.0f,	-1.0f,	// J (9)
+		1.5f,  0.0f, -3.0f,		0.52f,  0.82f,		0.0f,	1.0f,	-1.0f,	// F (10)
+		0.0f, -0.7f,  -4.0f,	0.625f,  0.70f,		0.0f,	1.0f,	-1.0f,	// C 
 
 		//x		y		z		S		T
-		1.5f,  0.0f, -3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// F
-		0.0f,  -2.5f,  0.0f,	0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// P 
-		3.0f,  0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	1.0f,	// H
+		1.5f,  0.0f, -3.0f,		0.98f,  0.82f,		1.0f,	1.0f,	-1.0f,	// F 
+		0.0f,  2.5f,  0.0f,		0.875f,  0.99f,		1.0f,	1.0f,	-1.0f,	// J (13)
+		3.0f,  0.0f,  0.5f,		0.77f,  0.82f,		1.0f,	1.0f,	-1.0f,	// H 
+		3.0f, -0.7f,  -2.0f,	0.875f,  0.70f,		1.0f,	1.0f,	-1.0f,	// O 
 
 		//x		y		z		S		T
-		3.0f,  0.0f,  0.5f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// H
-		0.0f,  -2.5f,  0.0f,	0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// P
-		0.0f, 0.0f,  3.0f,		0.0f,  0.0f,		0.0f,	0.0f,	-1.0f,	// E
+		3.0f,  0.0f,  0.5f,		0.23f,  0.47f,		1.0f,	1.0f,	1.0f,	// H (16)
+		0.0f,  2.5f,  0.0f,		0.115f,  0.62f,		1.0f,	1.0f,	1.0f,	// J (17)
+		0.0f, 0.0f,  3.0f,		0.0f,  0.47f,		1.0f,	1.0f,	1.0f,	// E 
+		2.0f, -0.7f, 2.0f,		0.115f,  0.35f,		1.0f,	1.0f,	1.0f,	// A 
+		 
+		//x		y		z		S		T			NX		NY		NZ
+		-2.0f, -0.7f, 2.0f,		0.27f,  0.53f,		0.0f,	-1.0f,	1.0f,	// B
+		0.0f, 0.0f,  3.0f,		0.375f,  0.62f,		0.0f,	-1.0f,	1.0f,	// E
+		2.0f, -0.7f, 2.0f,		0.48f,  0.53f,		0.0f,	-1.0f,	1.0f,	// A
+		0.0f,  -2.5f,  0.0f,	0.375f,  0.35f,		0.0f,	-1.0f,	1.0f,	// P
+
+		//x		y		z		S		T
+		-3.0f, -0.7f,  -2.0f,	0.52f,  0.55f,		-1.0f,	-1.0f,	0.0f,	// D
+		-3.0f, 0.0f,  0.5f,	    0.625f,  0.62f,		-1.0f,	-1.0f,	0.0f,	// G
+		-2.0f, -0.7f, 2.0f,		0.72f,  0.55f,		-1.0f,	-1.0f,	0.0f,	// B
+		0.0f,  -2.5f,  0.0f,	0.625f,  0.35f,		-1.0f,	-1.0f,	0.0f,	// P
+
+		//x		y		z		S		T
+		0.0f, -0.7f,  -4.0f,	0.77f,  0.57f,		0.0f,	0.0f,	0.0f,	// C
+		-1.5f, 0.0f,  -3.0f,	0.875f,  0.62f,		0.0f,	0.0f,	0.0f,	// K
+		-3.0f, -0.7f,  -2.0f,	0.98f,  0.57f,		0.0f,	0.0f,	0.0f,	// D
+		0.0f,  -2.5f,  0.0f,	0.875f,  0.35f,		0.0f,	0.0f,	0.0f,	// P
+
+		//x		y		z		S		T
+		3.0f, -0.7f,  -2.0f,	0.0f,  0.22f,		0.0f,	0.0f,	0.0f,	// O
+		1.5f,  0.0f, -3.0f,		0.115f,  0.30f,		0.0f,	0.0f,	0.0f,	// F 
+		0.0f, -0.7f,  -4.0f,	0.23f,  0.22f,		0.0f,	0.0f,	0.0f,	// C
+		0.0f,  -2.5f,  0.0f,	0.115f,  0.0f,		0.0f,	0.0f,	0.0f,	// P
+
+		//x		y		z		S		T
+		2.0f, -0.7f, 2.0f,		0.25f,  0.22f,		0.0f,	0.0f,	0.0f,	// A
+		3.0f,  0.0f,  0.5f,		0.375f,  0.30f,		0.0f,	0.0f,	0.0f,	// H
+		3.0f, -0.7f,  -2.0f,	0.50f,  0.22f,		0.0f,	0.0f,	0.0f,	// O
+		0.0f,  -2.5f,  0.0f,	0.375f,  0.0f,		0.0f,	0.0f,	0.0f,	// P
 	};
 
 	Mesh* dado = new Mesh();
-	dado->CreateMesh(cubo_vertices, cubo_indices, 240 , 30);
+	dado->CreateMesh(cubo_vertices, cubo_indices, 320, 60);
 	meshList.push_back(dado);
 }
 
@@ -278,7 +300,7 @@ int main()
 	CrearDado();
 	CreateShaders();
 
-	camera = Camera(glm::vec3(0.0f, 5.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
 
 	brickTexture = Texture("Textures/brick.png");
 	brickTexture.LoadTextureA();
@@ -293,13 +315,9 @@ int main()
 	logofiTexture = Texture("Textures/escudo_fi_color.tga");
 	logofiTexture.LoadTextureA();
 
-	dadon = Texture("Textures/dadonull.jpg");
-	dadon.LoadTexture();
-	dadonT = Texture("Textures/dadonullT.tga"); // El formato JPG no soporta el canal alpha
-	dadonT.LoadTextureA();
-	dado_A = Texture("Textures/dado_animales.tga");
-	dado_A.LoadTexture();
-	
+	Dado = Texture("Textures/DadoTen.tga");
+	Dado.LoadTexture();
+
 	
 	std::vector<std::string> skyboxFaces;
 	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
@@ -317,15 +335,23 @@ int main()
 	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
 	
 	glm::mat4 model(1.0);
+	glm::mat4 dado(1.0);
 	glm::mat4 modelaux(1.0);
 	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
 	////Loop mientras no se cierra la ventana
+	float grad = 0.0f; // Para rotar el dado
+	int seg, r1 = 1,r2 = 0,n = 0;
+	// Para medir el tiempo
+
 	while (!mainWindow.getShouldClose())
 	{
+
 		GLfloat now = glfwGetTime();
 		deltaTime = now - lastTime;
 		deltaTime += (now - lastTime) / limitFPS;
 		lastTime = now;
+
+		grad += 0.3f;
 
 		//Recibir eventos del usuario
 		glfwPollEvents();
@@ -341,7 +367,6 @@ int main()
 		uniformProjection = shaderList[0].GetProjectionLocation();
 		uniformView = shaderList[0].GetViewLocation();
 		uniformColor = shaderList[0].getColorLocation();
-
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
 		glUniform3f(uniformEyePosition, camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
@@ -350,6 +375,7 @@ int main()
 
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		modelaux = model;
 		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
@@ -357,27 +383,25 @@ int main()
 		pisoTexture.UseTexture();
 		meshList[2]->RenderMesh();
 
+		model = modelaux;
 
 		//Dado de Opengl
 		//Ejercicio 1: Texturizar su cubo con la imagen dado_animales ya optimizada por ustedes
-		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(-1.5f, 4.5f, -2.0f));
+
+		model = glm::translate(model, glm::vec3(-1.5f, 10.0f, -2.0f));
+		model = glm::rotate(model, glm::radians(grad), glm::vec3(0.0f, 1.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		//glEnable(GL_BLEND);
-		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		Dado.UseTexture();
 		meshList[4]->RenderMesh();
+		
+		
+
 		//glDisable(GL_BLEND);
 		
 		//Ejercicio 2:Importar el cubo texturizado en el programa de modelado con 
 		//la imagen dado_animales ya optimizada por ustedes
 		
-		////Dado importado
-		//model = glm::mat4(1.0);
-		//model = glm::translate(model, glm::vec3(-3.0f, 3.0f, -2.0f));
-		//model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
-		//glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		//Dado_A.RenderModel();
-		
+
 		glUseProgram(0);
 
 		mainWindow.swapBuffers();
